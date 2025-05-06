854c2e505566a0fb03445d82ca1c878c77a277ae
Util787 <georgiy.chuprina.787@mail.ru>
2025-05-06 23:24:03 +0300

    1st task

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+
+	RMQConnection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatal("Failed to create connection with AMPQ URI")
+	}
+	defer RMQConnection.Close()
+	log.Println("AMPQ URI connected successfully")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT)
+	sig := <-signalChan
+	log.Println("Received signal:", sig)
+	log.Println("Shutting down the programm")
+
+	//better this logic:
+	//make chan without buf
+	//signalChan := make(chan os.Signal) and:
+	//go func() {
+	//     <-signalChan
+	//     *some logic*
+	//     os.Exit(1)
+	// }()
 }
